632c319fab934d3265eb2bd8899af019ddfe33a2
uznog <konrad.baran224@gmail.com>
2021-03-16 23:58:23 +0100

    adding a few apps

--- config/home.nix
+++ config/home.nix
@@ -5,6 +5,7 @@ with pkgs.lib;
   imports = [
     ../modules/settings.nix
     ./programs
+    ./i3
   ];
 
   nixpkgs.config = import ./nixpkgs.nix;
@@ -13,15 +14,20 @@ with pkgs.lib;
     alacritty
     feh
     ffmpeg
+    ferdi
     firefox
     home-manager
     jq
     killall
+    lxappearance
     neovim
     pv
+    signal-desktop
     unzip
     zip
+    zoom-us
   ];
 
   xdg.configFile."alacritty/alacritty.yml".source = ./dotfiles/alacritty.yml;
+  xdg.configFile."diricons".source = ./dotfiles/lf-icons;
 }
