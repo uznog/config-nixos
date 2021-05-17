d6685e7d116137d1cb836de644f8a22c3df27667
Konrad Baran <konrad.baran@snow.dog>
2021-05-17 14:21:08 +0200

    moving nixpkgs config to upper levels

--- config/programs/devops.nix
+++ config/programs/devops.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config = import ../../nixpkgs.nix;
-
   home.packages = with pkgs; [
     ansible_2_9
     httpie
--- config/programs/k8s.nix
+++ config/programs/k8s.nix
@@ -5,8 +5,6 @@ let
 in
 with pkgs.lib;
 {
-  nixpkgs.config = import ../../nixpkgs.nix;
-
   home.packages = with unstable; [
     k9s
     google-cloud-sdk
