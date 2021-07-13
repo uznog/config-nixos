ef47d43a20f9646fce7f372a79c42c979f5e2c48
Konrad Baran <konrad.baran@snow.dog>
2021-07-13 19:18:03 +0200

    more opacity rules

--- config/wm/picom.nix
+++ config/wm/picom.nix
@@ -2,7 +2,7 @@
   services.picom = {
     enable = true;
 
-    activeOpacity = "0.9";
+    activeOpacity = "1.0";
 
     blur = true;
     blurExclude = [ "class_g = 'polybar'" ];
@@ -16,10 +16,14 @@
     opacityRule = [
       "100:class_g = 'Firefox'"
       "100:class_g = 'Ferdi'"
-      "100:class_g = 'discord'"
-      "100:class_g = 'zoom'"
+      "97:class_g = 'discord'"
+      "100:class_g = 'gimp'"
+      "100:class_g = 'i3lock'"
       "100:class_g = 'mpv'"
       "100:class_g = 'polybar'"
+      "100:class_g = '.virt-manager-wrapped'"
+      "100:class_g = 'Virt-viewer'"
+      "100:class_g = 'zoom'"
       "97:class_g = 'Alacritty' && focused"
       "80:class_g = 'Alacritty' && !focused"
       "97:class_g = 'Code' && focused"
@@ -29,6 +33,7 @@
     shadow = false;
     shadowExclude = [ "class_g = 'polybar'" ];
     shadowOpacity = "0.25";
+
 
   };
 }
