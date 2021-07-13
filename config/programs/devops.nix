6d621546907c7b2de0f90d4d74cbf460cfdc7c87
Konrad Baran <konrad.baran@snow.dog>
2021-07-13 19:12:54 +0200

    adding devops apps

--- config/programs/devops.nix
+++ config/programs/devops.nix
@@ -3,7 +3,13 @@
 {
   home.packages = with pkgs; [
     ansible_2_9
+    dbeaver
+    dig
     httpie
+    oracle-instantclient
+    terraform_0_15
     vagrant
+    virt-manager
+    virt-viewer
   ];
 }
