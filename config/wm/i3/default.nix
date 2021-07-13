74e650a8f1b615f9cb5212bb069e5e1a895425dc
Konrad Baran <konrad.baran@snow.dog>
2021-07-13 19:20:07 +0200

    modifying rofi shortcuts, added workaround for dead polybar on start

--- config/wm/i3/default.nix
+++ config/wm/i3/default.nix
@@ -25,9 +25,9 @@ with pkgs.lib;
       };
       keybindings = mkOptionDefault(
         {
-          "${mod}+space" = "exec rofi -show combi -show-icons";
-          "${mod}+Ctrl+space" = "exec rofi -show ssh -show-icons";
-          "${alt}+Tab" = "exec rofi -show window -show-icons";
+          "${mod}+space" = "exec rofi -dpi 1 -show combi -show-icons";
+          "${mod}+Ctrl+space" = "exec rofi -dpi 1 -show ssh -show-icons";
+          "${alt}+Tab" = "exec rofi -dpi 1 -show window -show-icons";
           "XF86AudioRaiseVolume" = "exec /home/${config.settings.username}/bin/volume.sh 2dB+";
           "XF86AudioLowerVolume" = "exec /home/${config.settings.username}/bin/volume.sh 2dB-";
           "XF86MonBrightnessUp" = "exec sudo xbacklight -inc 5";
@@ -57,6 +57,7 @@ with pkgs.lib;
       focus_wrapping no
       exec_always "if [[ -e $HOME/.background-image ]]; then feh --bg-scale $HOME/.background-image; fi"
       exec_always "autorandr -c"
+      exec_always "systemctl --user restart polybar.service"
       bindsym --release Mod4+Shift+Z exec "import png:- | tee /home/${config.settings.username}/usr/screenshots/screenshot_$(date +%F_%T).png | xclip -selection clipboard -t image/png"
       for_window [class="mpv"] floating enable
       for_window [class="firefox"] gaps left 0 right 0 bottom 0
