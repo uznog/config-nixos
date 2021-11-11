750d23951c1b3a47712f7bec8a92456c0a233dda
Konrad Baran <konrad.baran224@gmail.com>
2021-11-11 16:07:31 +0100

    sorting out settings module

--- modules/settings.nix
+++ modules/settings.nix
@@ -1,47 +1,79 @@
-{ config, pkgs, lib, ... }:
+{ lib, config, ... }:
 
 with lib;
-
 {
-  options = {
-    settings = {
+  options.settings = {
+
+    user = {
       name = mkOption {
-        default = "Konrad Baran";
         type = with types; uniq str;
-      };
-      username = mkOption {
         default = "kbaran";
-        type = with types; uniq str;
+        description = "User's username to be used within system";
       };
+
+      fullname = mkOption {
+        type = with types; uniq str;
+        default = "Konrad Baran";
+        description = "User's full ID";
+      };
+
       email = mkOption {
+        type = with types; uniq str;
         default = "konrad.baran224@gmail.com";
-        type = with types; uniq str;
+        description = "Email address to use";
       };
-      terminal = mkOption {
-        default = "alacritty";
-        type = with types; uniq str;
-      };
-      fontName = mkOption {
-        default = "SauceCodePro Nerd Font";
-        type = with types; uniq str;
-      };
-      fontSize = mkOption {
-        default = 12;
-        type = types.int;
-      };
-      kbLayout = mkOption {
-        default = "pl";
+
+      homeDir = mkOption {
         type = types.str;
-      };
-      xkbFile = mkOption {
-        default = "none";
-        type = with types; uniq str;
-        description = ''
-          Filename of the xkb file to load (or "none" if no keyboard
-          layout is desired). File is specified without extension and
-          must be present in the xkb directory.
-        '';
+        default = "/home/${config.settings.user.name}";
+        description = "Location of user's home directory";
       };
     };
+
+    host = {
+      isVirtual = mkOption {
+        type = types.bool;
+        default = false;
+        example = true;
+        description = "True if host is a virtual machine";
+      };
+
+      hostsFile = mkOption {
+        type = types.path;
+        default = config.settings.user.homeDir + "/etc/config-sensitive/hosts";
+        description = "Path to file containing hosts in /etc/hosts manner to be used";
+      };
+    };
+
+    terminal = mkOption {
+      type = with types; uniq str;
+      default = "alacritty";
+    };
+
+    fontName = mkOption {
+      type = types.str;
+      default = "SauceCodePro Nerd Font";
+    };
+
+    fontSize = mkOption {
+      type = types.int;
+      default = 12;
+    };
+
+    kbLayout = mkOption {
+      type = types.str;
+      default = "pl";
+    };
+
+    xkbFile = mkOption {
+      type = with types; uniq str;
+      default = "none";
+      description = ''
+        Filename of the xkb file to load (or "none" if no keyboard
+        layout is desired). File is specified without extension and
+        must be present in the xkb directory.
+      '';
+    };
+
   };
 }
