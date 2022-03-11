e76d9adfc90c37d42d490efdc063afbb6dfb7dbb
Konrad Baran <konrad.baran@snow.dog>
2022-03-11 07:17:48 +0100

    defined services for xps

--- hosts/xps/settings.nix
+++ hosts/xps/settings.nix
@@ -5,6 +5,11 @@
     user = {
       name = "kbaran";
     };
+    services = {
+      grafana.enable = true;
+      prometheus.enable = false;
+      traefik.enable = true;
+    };
     terminal = "alacritty";
     fontName = "SauceCodePro Nerd Font";
     fontSize = 12;
