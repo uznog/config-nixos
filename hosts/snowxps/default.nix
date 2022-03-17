f584a468fb17aa7164520ddbd5466817fcc454c4
Konrad Baran <konrad.baran@snow.dog>
2022-03-17 09:52:33 +0100

    modularizing up snowxps configuration

--- hosts/snowxps/default.nix
+++ hosts/snowxps/default.nix
@@ -4,6 +4,17 @@ inputs@{ config, pkgs, ... }:
   imports = [
     ./settings.nix
     ./hardware-configuration.nix
+
+    ../../modules/services/ssh.nix
+    ../../modules/services/strongswan.nix
+    ../../modules/services/pipewire.nix
+    ../../modules/services/docker.nix
+    ../../modules/services/libvirtd.nix
+    ../../modules/services/grafana.nix
+    ../../modules/services/prometheus.nix
+    ../../modules/services/traefik.nix
+
+    ../../modules/desktop/kde.nix
   ];
 
   boot.loader = {
@@ -29,7 +40,7 @@ inputs@{ config, pkgs, ... }:
       efiSysMountPoint = "/boot/efi";
     };
   };
-          
+
 
   boot.initrd.luks.devices = {
     root = {
@@ -46,61 +57,20 @@ inputs@{ config, pkgs, ... }:
   };
 
   boot.supportedFilesystems = [ "ntfs" ];
-  
+
   time.timeZone = "Europe/Warsaw";
 
   sound = {
-    enable = false;
     mediaKeys.enable = true;
-  };
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    media-session.config.bluez-monitor.rules = [
-      {
-        # Matches all cards
-        matches = [ { "device.name" = "~bluez_card.*"; } ];
-        actions = {
-          "update-props" = {
-            "bluez5.reconnect-profiles" = [ "a2dp_sink" "hfp_hf" "hsp_hs" ];
-            # mSBC is not expected to work on all headset + adapter combinations.
-            "bluez5.msbc-support" = true;
-            # SBC-XQ is not expected to work on all headset + adapter combinations.
-            "bluez5.sbc-xq-support" = true;
-          };
-        };
-      }
-      {
-        matches = [
-          # Matches all sources
-          { "node.name" = "~bluez_input.*"; }
-          # Matches all outputs
-          { "node.name" = "~bluez_output.*"; }
-        ];
-        actions = {
-          "node.pause-on-idle" = false;
-        };
-      }
-    ];
   };
 
   hardware.bluetooth = {
     enable = true;
-    settings = {
-      General = {
-        Enable="Source,Sink,Media,Socket";
-      };
-    };
   };
 
   networking = {
-    hostName = "xps";
+    hostName = "snowxps";
     useDHCP = false;
-    hostFiles = [ "${inputs.sensitive}/hosts" ];
 
     firewall = {
       enable = false;
@@ -111,31 +81,12 @@ inputs@{ config, pkgs, ... }:
 
     networkmanager = {
       enable = true;
-      enableStrongSwan = true;
-      packages = with pkgs; [
-        networkmanager-l2tp
-      ];
     };
   };
 
   console.useXkbConfig = true;
 
-  programs.ssh.ciphers = [
-    "aes128-cbc"
-    "3des-cbc"
-    "aes192-cbc"
-    "aes256-cbc"
-    "chacha20-poly1305@openssh.com"
-    "aes128-ctr"
-    "aes192-ctr"
-    "aes256-ctr"
-    "aes128-gcm@openssh.com"
-    "aes256-gcm@openssh.com"
-  ];
-
   services = {
-    cron.enable = true;
-    openssh.enable = true;
     blueman.enable = true;
 
     minio = {
@@ -145,85 +96,21 @@ inputs@{ config, pkgs, ... }:
       rootCredentialsFile = "${config.settings.user.homeDir}/etc/config-sensitive/minio/credentials";
     };
 
-    strongswan = {
-      enable = true;
-      secrets = [
-        "ipsec.d/ipsec.nm-l2tp.secrets"
-      ];
-    };
-
     tlp.enable = true;
 
-    xserver = {
-      enable = true;
-      autorun = true;
-
-      libinput = {
-        enable = true;
-      };
-
-      layout = "pl"; 
-      xkbOptions = "ctrl:nocaps";
-      libinput = {
-        touchpad = {
-          tapping = true;
-        };
-      };
-
-      desktopManager = {
-        xterm.enable = false;
-        plasma5.enable = true;
-      };
-
-      displayManager = {
-        autoLogin = {
-          enable = true;
-          user = "${config.settings.user.name}";
-        };
-        sddm = {
-          enable = true;
-        };
-      };
-
-    };
   };
 
   programs.atop = {
-    enable = true;
+    enable = false;
     atopService.enable = true;
     atopacctService.enable = true;
     atopgpu.enable = true;
-  };
-
-  virtualisation = {
-    docker = {
-      enable = true;
-      enableOnBoot = false;
-      liveRestore = false;
-    };
-
-    containerd.enable = true;
-
-    libvirtd = {
-      enable = true;
-      onBoot = "ignore";
-      qemu = {
-        runAsRoot = false;
-        ovmf = {
-          enable = true;
-          package = pkgs.OVMFFull;
-        };
-      };
-    };
   };
 
   security = {
     sudo = {
       enable = true;
       wheelNeedsPassword = false;
-    };
-    doas = {
-      enable = false;
     };
   };
 
