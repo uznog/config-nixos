10495b1ddb48fd1d8cc4b4e8b2f456a6291bc96a
Konrad Baran <konrad.baran@snow.dog>
2022-03-17 09:59:09 +0100

    added kde module

---
+++ modules/desktop/kde.nix
@@ -0,0 +1,31 @@
+{ config, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    autorun = true;
+
+    libinput = {
+      enable = true;
+      touchpad = {
+        tapping = true;
+      };
+    };
+
+    layout = "pl";
+    xkbOptions = "ctrl:nocaps";
+
+    desktopManager = {
+      plasma5.enable = true;
+    };
+
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "${config.settings.user.name}";
+      };
+      sddm.enable = true;
+    };
+  };
+
+}
