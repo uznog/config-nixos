83cc6c00b898b8e5f69cbf63062b0a310f38ee44
Konrad Baran <konrad.baran@snow.dog>
2022-03-17 09:53:27 +0100

    removing unused settings

--- hosts/snowxps/settings.nix
+++ hosts/snowxps/settings.nix
@@ -5,11 +5,6 @@
     user = {
       name = "kbaran";
     };
-    services = {
-      grafana.enable = true;
-      prometheus.enable = false;
-      traefik.enable = true;
-    };
     terminal = "alacritty";
     fontName = "SauceCodePro Nerd Font";
     fontSize = 12;
