2f0ca2a7fde573fe3f96fd5a92ddb40f424fc227
Konrad Baran <konrad.baran@snow.dog>
2022-03-17 10:02:25 +0100

    flake general overhaul

--- flake.nix
+++ flake.nix
@@ -1,70 +1,70 @@
 {
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/d5f237872975e6fb6f76eef1368b5634ffcd266f";
+    # nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
+    nixpkgs.url = github:NixOS/nixpkgs/fcd48a5a0693f016a5c370460d0c2a8243b882dc;
 
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixos-stable.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixos-hardware.url = github:NixOS/nixos-hardware;
 
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
+    utils.url = github:gytis-ivaskevicius/flake-utils-plus/v1.3.1;
 
-    agenix.url = "github:ryantm/agenix";
+    sops-nix.url = github:Mic92/sops-nix;
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = github:nix-community/home-manager;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    sensitive.url = "path:/etc/nixos/sensitive";
-    sensitive.flake = false;
-
-    dotfiles.url = "github:uznog/dotfiles";
+    dotfiles.url = github:uznog/dotfiles;
     dotfiles.flake = false;
   };
 
   outputs = inputs@
   { self
-  , utils, agenix 
+  , utils
   , nixpkgs, nixos-hardware, home-manager
-  ,  dotfiles, sensitive, ... }:
+  , sops-nix, dotfiles, ... }:
 
   let
     inherit (utils.lib) mkFlake exportModules;
-    pkgs = self.pkgs.x86_64-linux.nixpkgs;
   in
   mkFlake rec {
     inherit self inputs;
 
+    nix = {
+      generateNixPathFromInputs = true;
+      generateRegistryFromInputs = true;
+    };
+
     supportedSystems = [ "x86_64-linux" ];
 
-    overlay = import ./overlays;
 
     channelsConfig = {
       allowUnfree = true;
-      allowUnsupportedSystem = true;
     };
+
+    overlay = import ./overlays;
 
     channels.nixpkgs.overlaysBuilder = channels: [
       self.overlay
     ];
 
     hostDefaults.modules = [
+      home-manager.nixosModules.home-manager
+      { home-manager.useGlobalPkgs = true; }
+      sops-nix.nixosModules.sops
+
       ./common.nix
       ./modules/settings.nix
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
+      ./modules/sops.nix
     ];
 
     hosts = {
-      nixos = {
+      snowxps = {
         system = "x86_64-linux";
         modules = [
-          ./hosts/xps/configuration.nix
+          ./hosts/snowxps
           nixos-hardware.nixosModules.dell-xps-15-9500-nvidia
-          agenix.nixosModule
         ];
-        specialArgs = { inherit dotfiles sensitive; };
+        specialArgs = { inherit inputs dotfiles; };
       };
     };
 
@@ -72,8 +72,8 @@
       devShell = mkShell {
         name = "sysconfig";
         buildInputs = [
-          agenix.defaultPackage.x86_64-linux
           git
+          sops
         ];
       };
     };
