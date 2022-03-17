88e4a00cd9ec85858fb911d3c5bfdddec528ac84
Konrad Baran <konrad.baran@snow.dog>
2022-03-17 09:54:19 +0100

    added default overlay imports

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,10 @@
 final: prev:
 
 {
-  gitkraken = final.callPackage ./gitkraken.nix {};
+  gitkraken = final.callPackage ./apps/gitkraken.nix {};
+
+  boot2windows = final.callPackage ./scripts/boot2windows {};
+  tidal = final.callPackage ./scripts/tidal {};
+  edit-l2tp-vpn = final.callPackage ./scripts/edit-l2tp-vpn {};
+  run-minio = final.callPackage ./scripts/run-minio {};
 }
