9e13ad7169d8321432b55d64150e176577429f5e
Konrad Baran <konrad.baran@snow.dog>
2022-03-27 21:03:32 +0200

    removed gitkraken overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,6 @@
 final: prev:
 
 {
-  gitkraken = final.callPackage ./apps/gitkraken.nix {};
-
   boot2windows = final.callPackage ./scripts/boot2windows {};
   tidal = final.callPackage ./scripts/tidal {};
   edit-l2tp-vpn = final.callPackage ./scripts/edit-l2tp-vpn {};
